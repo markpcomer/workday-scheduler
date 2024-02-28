ed6301ba56b8197526f37aa508cd1e1e0e66cd09
markpcomer <markpcomer@gmail.com>
2024-02-27 23:31:11 -0500

    Further Commit

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var currentDayEl = $('#currentDay');
-$(function () {
+$(document).ready(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -21,13 +21,19 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+
+    
+
+
+
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
+    
     //
     // TODO: Add code to display the current date in the header of the page.
-    var now = dayjs().format("dddd, MMMM DD, YYYY");
+    var now = dayjs().format("dddd, MMMM D, YYYY");
     currentDayEl.text(now);
 
   });

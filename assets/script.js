7803405932c41cf22f3a8eb684835ef30ef90247
markpcomer <markpcomer@gmail.com>
2024-02-27 21:36:28 -0500

    Added Current Date to Header

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDayEl = $('#currentDay');
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +9,12 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        
+        
+    });
     //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,5 +27,8 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    var now = dayjs().format("dddd, MMMM DD, YYYY");
+    currentDayEl.text(now);
+
   });
   
